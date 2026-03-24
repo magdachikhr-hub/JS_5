00f853358f24290f3c83a7585a910f3e378df354
Magda Chikhradze <magda292005@gmail.com>
2026-03-24 18:20:24 +0400

    else if

--- script.js
+++ script.js
@@ -49,3 +49,95 @@ console.log(exampleTwo);
 
 console.log(typeof (200 / "2"));
 console.log("200" - "2");
+
+//ifs
+
+if (10 > 20) {
+  console.log("true");
+}
+
+if (20 > 10) {
+  console.log("this condition is true");
+}
+
+let myAge = 20;
+if (myAge >= 18) {
+  console.log("customer is of legal age");
+}
+
+let myAge2 = 17;
+if (myAge2 >= 18) {
+  console.log("customer is of legal age");
+} else {
+  console.log("customer is not of legal age");
+}
+
+console.log("continue");
+
+//
+let myAge3 = -3;
+
+if (myAge3 >= 18 && myAge3 < 120) {
+  console.log("customer is of legal age");
+} else if (myAge3 > 0 && myAge3 < 18) {
+  console.log("customer is not of legal age");
+} else {
+  console.log("the given age is incorrect");
+}
+
+//&& - მის გვერდებზე მდგომი ორივე პირობა უნდა შესრულდეს რომ ჭეშმარიტი გამოვიდეს (ლოგიკური და)
+console.log(2 === 2 && 3 > 0);
+
+//
+
+let score = 100;
+
+// if (score > 90 && score <= 100) {
+//   console.log("A");
+// } else if (score > 100) {
+//   console.log("INCORRECT NUMBER");
+// } else if (score < 0) {
+//   console.log("INCORRECT NUMBER");
+// } else if (score >= 80) {
+//   console.log("B");
+// } else if (score > 70) {
+//   console.log("C");
+// } else if (score > 60) {
+//   console.log("D");
+// } else if (score > 50) {
+//   console.log("F");
+// } else {
+//   console.log("FX");
+// }
+
+if (score <= 100 && score >= 90) {
+  console.log("A");
+} else if (score >= 80 && score < 90) {
+  console.log("B");
+} else if (score >= 70 && score < 80) {
+  console.log("C");
+} else if (score >= 60 && score < 70) {
+  console.log("D");
+} else if (score >= 50 && score < 60) {
+  console.log("F");
+} else if (score >= 0 && score < 40) {
+  console.log("FX");
+} else {
+  console.log("INCORRECT NUMBER");
+}
+
+if (score > 100 && score < 0) {
+  console.log("INCORRECT NUMBER");
+} else if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else if (score >= 60) {
+  console.log("D");
+} else if (score >= 50) {
+  console.log("F");
+} else {
+  console.log("FX");
+}
