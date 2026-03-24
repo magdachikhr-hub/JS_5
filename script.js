f8832094b04537d96745648dcaa9aa1737b977b7
Magda Chikhradze <magda292005@gmail.com>
2026-03-24 16:33:12 +0400

    ===

---
+++ script.js
@@ -0,0 +1,40 @@
+//JS_6
+// = - მინიჭების ოპერატორი (ერთმაგი ტოლობა)
+let number = 20;
+
+// == - მნიშვნელობების შედარება (ორმაგი ტოლობა)
+let age = 23;
+let yourAge = 30;
+let world = "23";
+console.log(age == yourAge);
+console.log(age == world);
+
+// === - მნიშვნელობებს და ტიპებს ადარებს (სამაგი ტოლობა) (ტიპი არის განსხვავებული - აბრუნებს უარყოფითს)
+
+let chosenAge = "40";
+
+console.log(age === chosenAge);
+
+// practice
+
+let text1 = "this is some text";
+let text2 = "this is some text";
+
+console.log(text1 == text2);
+console.log(text1 === text2);
+
+// მეტობა ნაკლებობა და ტოლობა
+
+console.log(10 > 20);
+console.log(20 >= 10);
+
+//ნაშთი
+console.log(20 % 8);
+
+console.log(20 % 8 === 0);
+
+console.log(30 % 11);
+
+//
+let compare = 30 === 20;
+console.log(compare);
